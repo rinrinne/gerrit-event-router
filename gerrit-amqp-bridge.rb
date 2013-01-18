edd773428d42f088a39c15cb0539dcf9b229044b
rinrinne <rinrin.ne@gmail.com>
2013-01-18 20:02:34 +0900

    First commit - script

---
+++ gerrit-amqp-bridge.rb
@@ -0,0 +1,58 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'eventmachine'
+require 'em-ssh'
+require 'amqp'
+require 'optparse'
+
+Version = '1.0'
+OPTS = {}
+
+begin
+  OptionParser.new do |opt|
+
+    opt.on('-n NAME', '--name', 'Name in config') do |v|
+      OPTS[:name] = v
+    end
+
+    opt.on('-c CONFIGFILE', '--config', 'Path to config file') do |v|
+      OPTS[:config] = v
+    end
+
+    opt.parse(ARGV)
+  end
+rescue OptionParser::ParseError => e
+  $stderr.puts e
+end
+
+abort('-a is not specified.') unless OPTS[:name]
+OPT[:config] = '/etc/gerrit-amqp-bridge.conf' unless OPTS[:config]
+
+EM.run do
+  Signal.trap("INT") do
+    EM.stop
+  end
+
+  AMQP.connect(:host => 'localhost') do |amqp_conn|
+    amqp_ch = AMQP::Channel.new(amqp_conn)
+    amqp_ex = amqp_ch.fanout("gerrit.event")
+    amqp_ch.queue("test").bind(amqp_ex)
+
+    EM::Ssh.start('localhost', 'testuser', :port => 29418) do |connection|
+      connection.errback do |err|
+        $stderr.puts "#{err} (#{err.class})"
+      end
+
+      connection.callback do |session|
+        session.exec('gerrit stream-events') do |channel, stream, data|
+          channel.on_data do |ch, data|
+            str = %Q({"host":"localhost","user":"testuser","event":#{data.strip}})
+            amqp_ex.publish(str)
+            $stdout.puts str
+          end
+        end
+      end
+    end
+  end
+end
