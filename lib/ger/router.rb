bcfb4cfb4b15a2493083f214639eea88a216d46c
rinrinne <rinrin.ne@gmail.com>
2013-02-12 12:07:43 +0900

    Change name 2nd phase

--- lib/ger/router.rb
+++ lib/ger/router.rb
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-module GerritEventBridge
-  class Bridge
+module GerritEventRouter
+  class Router
     def initialize(name, config)
       @name = name
       begin
@@ -9,7 +9,7 @@ module GerritEventBridge
         @broker = config.brokers[@gerrit.broker]
         raise "Broker name is not found: #{@gerrit.broker}" unless @broker
 
-        GEB.logger.info "Configured bridge: #{@gerrit.header}(#{@gerrit.name}) -> broker#{@broker.header}(#{@broker.name})"
+        GER.logger.info "Configured bridge: #{@gerrit.header}(#{@gerrit.name}) -> broker#{@broker.header}(#{@broker.name})"
         @configured = true
       rescue
         @configured = false
@@ -18,24 +18,24 @@ module GerritEventBridge
     end
 
     def start
-      raise "Bridge still not be configured" unless @configured
+      raise "Router still not be configured" unless @configured
 
       Signal.trap(:INT) do
-        GEB.logger.info "Receive signal: INT. terminating."
+        GER.logger.info "Receive signal: INT. terminating."
         EM.stop
       end
 
       Signal.trap(:TERM) do
-        GEB.logger.info "Receive signal: TERM. terminating."
+        GER.logger.info "Receive signal: TERM. terminating."
         EM.stop
       end
 
       Signal.trap(:USR2) do
-        GEB.logger.debug "Receive signal: USR2"
-        if GEB.logger.level == GEB::LOG_NORMAL then
-          GEB.logger.level = GEB::LOG_DEBUG
+        GER.logger.debug "Receive signal: USR2"
+        if GER.logger.level == GER::LOG_NORMAL then
+          GER.logger.level = GER::LOG_DEBUG
         else
-          GEB.logger.level = GEB::LOG_NORMAL
+          GER.logger.level = GER::LOG_NORMAL
         end
       end
 
@@ -48,20 +48,20 @@ module GerritEventBridge
         EM.run do
           EM::Ssh.start(uri.host, uri.user, ssh_options) do |connection|
             connection.errback do |err|
-              GEB.logger.error { "#{@gerrit.header} #{err} (#{err.class})" }
+              GER.logger.error { "#{@gerrit.header} #{err} (#{err.class})" }
               EM.stop
             end
 
             connection.callback do |session|
-              GEB.logger.info("#{@gerrit.header} connection established.")
+              GER.logger.info("#{@gerrit.header} connection established.")
 
-              GEB::Broker.connect(@broker) do |broker|
-                GEB.logger.info("#{@broker.header} connection established.")
-                GEB.logger.debug("#{@broker.header} channel id = #{broker.channel.id}")
+              GER::Broker.connect(@broker) do |broker|
+                GER.logger.info("#{@broker.header} connection established.")
+                GER.logger.debug("#{@broker.header} channel id = #{broker.channel.id}")
 
                 session.exec(@gerrit.command) do |channel, stream, data|
                   channel.on_data do |ch, data|
-                    str = %Q({"version":"#{GEB::SCHEMA_VERSION}","host":"#{uri.host}","user":"#{uri.user}","event":#{data.strip}})
+                    str = %Q({"version":"#{GER::SCHEMA_VERSION}","host":"#{uri.host}","user":"#{uri.user}","event":#{data.strip}})
                     broker.send(str, :routing_key => @gerrit.routing_key)
                   end
                 end
@@ -70,8 +70,8 @@ module GerritEventBridge
           end
         end
       rescue => e
-        GEB.logger.error { "#{e} (#{e.class})" }
-        GEB.logger.debug { e.backtrace.join("\n") }
+        GER.logger.error { "#{e} (#{e.class})" }
+        GER.logger.debug { e.backtrace.join("\n") }
         EM.stop
       end
     end
